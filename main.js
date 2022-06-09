b91a362997560585506473e443c394b62c6825b4
yiqing hao <1337074512@qq.com>
2022-06-09 17:49:37 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import 'reveal.js/dist/reveal.css'
+import 'reveal.js/dist/theme/white.css'
+import Reveal from 'reveal.js'
+
+const deck = new Reveal()
+deck.initialize({
+  hash: true,
+  slideNumber: false
+})
+
